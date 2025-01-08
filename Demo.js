1fd037b50244ce75abf0de4edce29ca2909b4c76
jayawickrama <nishantharaka2004@gmail.com>
2025-01-08 11:50:25 +0530

    if cinditions

--- Demo.js
+++ Demo.js
@@ -2,10 +2,18 @@ import http from 'http'
 import fs from 'fs'
 
 const server =http.createServer((req,res)=>{
-    console.log(req.url,req.method)
-    res.getHeader('content-type','text/html')
-    res.write('<html><head><h1>hello world</h1></head></html>')
+    if(req.url == ""){
+        res.write('<html><head><h1>Welcome to the System</h1></head></html>')
+        res.end();
+    }
+    if(req.url == "/"){
+        res.write('<html><head><h1>Dashboard</h1></head></html>')
+        res.end();
+    }if(req.url == "/add"){
+        res.write('<html><head><h1>Add form</h1></head></html>')
+        res.end();
+    }
 });
 
 
-server.listen(3000)
+server.listen(3000);
